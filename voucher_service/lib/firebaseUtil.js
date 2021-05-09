be6e67916f1465682c0f0985622b58675c7d93b1
FungusChef <62349132+FungusChef@users.noreply.github.com>
2021-05-09 23:39:27 +1000

    Update firebaseUtil.js

--- voucher_service/lib/firebaseUtil.js
+++ voucher_service/lib/firebaseUtil.js
@@ -1,5 +1,5 @@
 export async function getAdminUID(firebase) {
-    const data = {key:null, value:null};
+    const data = {key:null, value:null}
 
     const dbRef = firebase.database().ref();
     await dbRef.child("Admin").child("Admin").get().then((snapshot) => {
@@ -13,21 +13,4 @@ export async function getAdminUID(firebase) {
         console.log(error);
     });
     return data;
-}
-
-export async function updateUserInfo(firebase) {
-    const uid = firebase.auth().currentUser.uid;
-    const dbRef = firebase.database().ref().child("users").child(String(uid));
-
-    // update personal info (can't update email)
-    await dbRef.child("personalInfo").update({
-        contactNumber: firebase.auth().currentUser.contactNumber,
-        name: firebase.auth().currentUser.displayName
-    });
-
-    // update billing info
-    await dbRef.child("billerInfo").update({
-        billerEmailAddress: firebase.auth().currentUser.billerEmailAddress,
-        invoiceName: firebase.auth().currentUser.displayName
-    });
 }
