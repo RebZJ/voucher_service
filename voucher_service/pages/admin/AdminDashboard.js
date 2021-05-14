3888b1b7552ed6f27728113be2151875cbeae843
qunzhi wang <adanwzy@gmail.com>
2021-05-14 21:03:45 +1000

    refactor add new service type function

--- voucher_service/pages/admin/AdminDashboard.js
+++ voucher_service/pages/admin/AdminDashboard.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { AddNewServiceType } from "../../components/AddNewServiceType";
 
 export default function AdminDashboard({ firebase }) {
   const router = useRouter();
@@ -12,6 +13,7 @@ export default function AdminDashboard({ firebase }) {
     <div>
       <h1>Admin Dashboard</h1>
       <p>Welcome admin! You are now signed in!</p>
+      <AddNewServiceType />
       <button onClick={() => logout()}>Logout</button>
     </div>
   );
